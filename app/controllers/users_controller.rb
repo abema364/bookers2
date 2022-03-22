f6abcb327dbc1239481321124efd199715812ab8
EC2 Default User <ec2-user@ip-172-31-38-213.ec2.internal>
2022-03-22 11:51:36 +0000

    [Add]ユーザー認証

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduc)
+  end
+
 end
